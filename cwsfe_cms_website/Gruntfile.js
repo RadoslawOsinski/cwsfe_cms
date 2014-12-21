06771ba8501ca3efd98b2e0f9045edfdaa557ad1
Radosław Osiński <Radoslaw.Osinski@cwsfe.pl>
2014-12-21 17:45:53 +0100

    Grunt watch module

--- cwsfe_cms_website/Gruntfile.js
+++ cwsfe_cms_website/Gruntfile.js
@@ -14,14 +14,25 @@ module.exports = function(grunt) {
                 // our nice reporter
                 reporter: require('jshint-stylish')
             }
+        },
+        watch: {
+            scripts: {
+                files: ['src/main/webapp/resources-cwsfe-cms/js/cms/**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    spawn: false
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', [
         // execute jshint on file
-        'jshint'
+        'jshint',
+        'watch'
     ]);
 
 };
