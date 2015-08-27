e85ff57743aaa6fa6916ba23d73351003e61ea38
Radosław Osiński <Radoslaw.Osinski@cwsfe.pl>
2015-08-27 21:33:44 +0200

    Parametrized test example

---
+++ cwsfe_cms_website/src/test/java/eu/com/cwsfe/cms/web/newsletter/EmailValidatorTest.java
@@ -0,0 +1,43 @@
+package eu.com.cwsfe.cms.web.newsletter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Radoslaw Osinski.
+ */
+@RunWith(value = Parameterized.class)
+public class EmailValidatorTest {
+
+    private String emailId;
+    private boolean expected;
+
+    public EmailValidatorTest(String emailId, boolean expected) {
+        this.emailId = emailId;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}: isValid({0})={1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                        {"mary@testdomain.com", true},
+                        {"mary.smith@testdomain.com", true},
+                        {"mary_smith123@testdomain.com", true},
+                        {"mary@testdomaindotcom", false},
+                        {"mary-smith@testdomain", false},
+                        {"testdomain.com", false},
+                        {"Radoslaw.Osinski@gmail.com", true}
+                }
+        );
+    }
+
+    @Test
+    public void testIsValidEmailAddress() throws Exception {
+        assertEquals(expected, EmailValidator.isValidEmailAddress(emailId));
+    }
+}
