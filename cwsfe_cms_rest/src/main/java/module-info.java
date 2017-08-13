c4ee6312703bb491ba947c8789b8738664e2cb45
Radosław Osiński <Radoslaw.Osinski@gmail.com>
2017-08-13 16:13:36 +0200

    Rest module migrated to JDK 9.

---
+++ cwsfe_cms_rest/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module eu.com.cwsfe.cms.rest {
+    requires eu.com.cwsfe.cms.services;
+
+    requires spring.beans;
+    requires spring.tx;
+    requires spring.web;
+    requires javax.servlet.api;
+    requires slf4j.api;
+}
