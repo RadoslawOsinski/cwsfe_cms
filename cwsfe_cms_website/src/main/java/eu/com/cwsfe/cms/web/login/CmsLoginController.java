f8e14f656d069bf5fdd94d916af7592c88bc9843
Radosław Osiński <Radoslaw.Osinski@gmail.com>
2017-08-20 18:22:49 +0200

    Server push example.

--- cwsfe_cms_website/src/main/java/eu/com/cwsfe/cms/web/login/CmsLoginController.java
+++ cwsfe_cms_website/src/main/java/eu/com/cwsfe/cms/web/login/CmsLoginController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.PushBuilder;
 import java.util.Optional;
 
 /**
@@ -28,14 +30,30 @@ class CmsLoginController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String loginPage(ModelMap model) {
+    public String loginPage(ModelMap model, HttpServletRequest req) {
         addMainSiteUrl(model);
+//        pushJsAndCssForLoginPage(req);
         return "cms/login/Login";
     }
 
+//    private void pushJsAndCssForLoginPage(HttpServletRequest req) {
+//        PushBuilder pushBuilder = req.newPushBuilder();
+//        if (pushBuilder != null) {
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/css/foundation/normalize.css").addHeader("content-type", "text/css").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/css/foundation/foundation.css").addHeader("content-type", "text/css").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/css/cms/shared.css").addHeader("content-type", "text/css").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/css/cms/colors.css").addHeader("content-type", "text/css").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/css/login/login.css").addHeader("content-type", "text/css").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/js/jquery/jquery-2.1.1.min.js").addHeader("content-type", "application/js").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/js/foundation/foundation.min.js").addHeader("content-type", "application/js").push();
+//            pushBuilder.path(req.getContextPath() + "/resources-cwsfe-cms/CWSFE_logo.png").addHeader("content-type", "application/js").push();
+//        }
+//    }
+
     @RequestMapping(value = "/loginPage")
-    public String login(ModelMap model) {
+    public String login(ModelMap model, HttpServletRequest req) {
         addMainSiteUrl(model);
+//        pushJsAndCssForLoginPage(req);
         return "cms/login/Login";
     }
 
